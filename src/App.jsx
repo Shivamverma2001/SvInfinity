8735bb28e2363ef85884c692908f1a6f9fa65fd8
Shivam verma <shivamverma@Shivams-MacBook-Air-3.local>
2024-08-24 17:33:17 +0530

    Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar/Sidebar'
+import Main from './Components/Main/Main'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
+    <div className='app-container'>
       <Sidebar/>
+      <Main/>
     </div>
   )
 }
