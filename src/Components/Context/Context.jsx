0486f52335ea4384607d31f71b2bb50046d486cf
Shivam verma <shivamverma@Shivams-MacBook-Air-3.local>
2024-08-25 10:32:22 +0530

    modify logic with apply typing affect in response

--- src/Components/Context/Context.jsx
+++ src/Components/Context/Context.jsx
@@ -12,13 +12,33 @@ const ContextProvider = (props) =>{
     const[loading,setLoading] = useState(false);
     const[resultData,setResultData] = useState("");
 
+    const delayParam = (index, nextWord) => {
+        setTimeout(function(){
+            setResultData(prev=>prev+nextWord)
+        },75*index)
+    }
+
     const onSent = async (prompt) =>{
         setResultData("");
         setLoading(true);
         setShowResult(true);
         setRecentPrompt(input);
         const response = await run(input);
-        setResultData(response);
+        let responesArray = response.split("**");
+        let newResponse="";
+        for(let i=0;i<responesArray.length;i++){
+            if(i===0 || i%2!==1){
+                newResponse+=responesArray[i];
+            }else{
+                newResponse+="<b>"+responesArray[i]+"</b>";
+            }
+        }
+        newResponse=newResponse.split("*").join("</br>")
+        let newResponseArray = newResponse.split(" ");
+        for(let i=0;i<newResponse.length;i++){
+            const nextWord=newResponseArray[i];
+            delayParam(i,nextWord+" ");
+        }
         setLoading(false);
         setInput("");
     }
